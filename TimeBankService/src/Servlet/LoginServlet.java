7e4902d0409ce2e142729e655758a3af75c3b6dc
fycnb <1162054822@qq.com>
2018-01-27 17:28:19 +0800

    change 01

--- TimeBankService/src/Servlet/LoginServlet.java
+++ TimeBankService/src/Servlet/LoginServlet.java
@@ -3,6 +3,7 @@ package Servlet;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.ResultSet;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,49 +12,28 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.alibaba.fastjson.JSONObject;
 
+import db.DBOper;
+
 public class LoginServlet extends HttpServlet {
 
-	/**
-	 * Constructor of the object.
-	 */
+
 	public LoginServlet() {
 		super();
 	}
 
-	/**
-	 * Destruction of the servlet. <br>
-	 */
+
 	public void destroy() {
-		super.destroy(); // Just puts "destroy" string in log
-		// Put your code here
+		super.destroy();
 	}
 
-	/**
-	 * The doGet method of the servlet. <br>
-	 *
-	 * This method is called when a form has its tag value method equals to get.
-	 * 
-	 * @param request the request send by the client to the server
-	 * @param response the response send by the server to the client
-	 * @throws ServletException if an error occurred
-	 * @throws IOException if an error occurred
-	 */
+
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
 		doPost(request, response);
 	}
 
-	/**
-	 * The doPost method of the servlet. <br>
-	 *
-	 * This method is called when a form has its tag value method equals to post.
-	 * 
-	 * @param request the request send by the client to the server
-	 * @param response the response send by the server to the client
-	 * @throws ServletException if an error occurred
-	 * @throws IOException if an error occurred
-	 */
+
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		StringBuffer sb = new StringBuffer();
@@ -76,12 +56,26 @@ public class LoginServlet extends HttpServlet {
         System.out.println(password+account);
 		response.setContentType("text/html");
 
-		String a= "123";
-		if("zhangsan".equals(account) && a.equals(password)){
-			response.getOutputStream().write("µÇÂ¼³É¹¦".getBytes("UTF-8"));
-		}else{
-			response.getOutputStream().write("µÇÂ¼Ê§°Ü".getBytes("UTF-8"));
-		}
+		DBOper db = new DBOper();
+		try
+			{
+			db.getConn();
+			String sql="select Userid from Users where account= ? and mypassword= ?";
+			ResultSet rs= db.executeQuery(sql, new String[]{ account,password });
+			if(rs.next()){
+				
+				String volnum = new String(rs.getString("volnum"));
+				System.out.print(volnum);
+				
+				response.getOutputStream().write(volnum.getBytes("utf-8"));
+
+			}else {
+				response.getOutputStream().write("µÇÂ¼Ê§°Ü".getBytes("utf-8"));
+
+			}	
+		} catch(Exception e) {
+    	e.printStackTrace();
+		} 
 		
 	}
 
