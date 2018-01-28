161b055554baf2dd072168ff9e6e21acdc503f6c
Mayizono <xynstudy@163.com>
2018-01-28 17:09:38 +0800

    tabhost
    超级丑

--- TimeBankAPP/gen/com/example/timebankapp/R.java
+++ TimeBankAPP/gen/com/example/timebankapp/R.java
@@ -10,6 +10,9 @@ package com.example.timebankapp;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int white=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -18,34 +21,54 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
-        public static final int checkboxHolder=0x7f080003;
-        public static final int clicklogin=0x7f080005;
-        public static final int etPassword=0x7f080002;
-        public static final int etUsername=0x7f080001;
-        public static final int loginButtonHolder=0x7f080004;
-        public static final int textFieldHolder=0x7f080000;
+        public static final int action_settings=0x7f090008;
+        public static final int checkboxHolder=0x7f090003;
+        public static final int clicklogin=0x7f090005;
+        public static final int etPassword=0x7f090002;
+        public static final int etUsername=0x7f090001;
+        public static final int iv_icon=0x7f090006;
+        public static final int loginButtonHolder=0x7f090004;
+        public static final int textFieldHolder=0x7f090000;
+        public static final int tv_title=0x7f090007;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_dh=0x7f030000;
+        public static final int activity_first=0x7f030001;
+        public static final int activity_login=0x7f030002;
+        public static final int activity_main=0x7f030003;
+        public static final int activity_my=0x7f030004;
+        public static final int activity_news=0x7f030005;
+        public static final int tab_icon=0x7f030006;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int dh=0x7f080000;
+        public static final int first=0x7f080001;
+        public static final int login=0x7f080002;
+        public static final int main=0x7f080003;
+        public static final int my=0x7f080004;
+        public static final int news=0x7f080005;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int action_sign_in=0x7f050005;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
-        public static final int user_name=0x7f050003;
-        public static final int user_password=0x7f050004;
+        public static final int action_settings=0x7f060002;
+        public static final int action_sign_in=0x7f060005;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060001;
+        public static final int title_activity_dh=0x7f06000b;
+        public static final int title_activity_first=0x7f060009;
+        public static final int title_activity_login=0x7f060007;
+        public static final int title_activity_main=0x7f060008;
+        public static final int title_activity_main1=0x7f060006;
+        public static final int title_activity_my=0x7f06000c;
+        public static final int title_activity_news=0x7f06000a;
+        public static final int user_name=0x7f060003;
+        public static final int user_password=0x7f060004;
     }
     public static final class style {
         /** 
@@ -69,10 +92,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- TimeBankAPP/src/com/example/timebankapp/MainActivity.java
+++ TimeBankAPP/src/com/example/timebankapp/MainActivity.java
@@ -1,68 +1,77 @@
 package com.example.timebankapp;
 
 import android.app.Activity;
+import android.app.TabActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.ImageView;
+import android.widget.TabHost;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
-	 private EditText etaccount;
-	    private EditText etPassword;
-	    private Button btnlogin;
-	
+public class MainActivity extends TabActivity {
 
+//	private RadioButton guide_home, guide_store, guide_cart,guide_ww;
+    private Intent intent1;
+    private Intent intent2;
+    private Intent intent3;
+    private Intent intent4;
+    private String tab="tab0";
+//    private int currIndex = 0;
+    private TabHost tabHost;
+    
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		 etaccount = (EditText) findViewById(R.id.etUsername);
-	        etPassword = (EditText) findViewById(R.id.etPassword);
-	        btnlogin = (Button)findViewById(R.id.clicklogin);
-	        btnlogin.setOnClickListener(new View.OnClickListener() {
-	            @Override
-	            public void onClick(View view) {
-//	                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
-//	                startActivity(intent);
-	                final String account = etaccount.getText().toString().trim();
-	                final String password = etPassword.getText().toString().trim();
-
-	                new Thread(){
-	                    public void run(){
-	                        final String result = LoginService.LoginByPost(account,password);
-//	                        System.out.println(result);
-	                        if(result != null) {
-	                            if (result.equals("登录成功")) {
-	                                
-	                                runOnUiThread(new Runnable() {
-	                                    @Override
-	                                    public void run() {
-	                                        Toast.makeText(MainActivity.this, result, 0).show();
-	                                      
-	                                        
-	                                    }
-	                                });
-	                            } else {
-	                                //请求失败，弹出toast
-	                                runOnUiThread(new Runnable() {
-	                                  
-	                                    public void run() {
-	                                        Toast.makeText(MainActivity.this, "账号或密码错误", 0).show();
-
-	                                    }
-	                                });
-	                            }
-	                        }
-	                    }
-	                }.start();
-	            }
-	        });
-
+        //初始化四个Activity
+        intent1 = new Intent(this, FirstActivity.class);
+        intent2 = new Intent(this, NewsActivity.class);
+        intent3 = new Intent(this, DHActivity.class);
+        intent4 = new Intent(this, MYActivity.class);
+//        init();
+        inittAB();
 	}
+    //填充TabHost
+    private void inittAB() {
+        tabHost = getTabHost();
+        //这里tab0是第一个，tab1是第二个窗口，以此类推
+        tabHost.addTab(tabHost.newTabSpec("首页")
+                .setIndicator(createTabIcon(R.drawable.ic_launcher, "首页"))
+                .setContent(intent1));
+        tabHost.addTab(tabHost.newTabSpec("消息")
+                .setIndicator(createTabIcon(R.drawable.ic_launcher, "消息"))
+                .setContent(intent2));
+        tabHost.addTab(tabHost.newTabSpec("兑换")
+                .setIndicator(createTabIcon(R.drawable.ic_launcher, "兑换"))
+                .setContent(intent4));
+        tabHost.addTab(tabHost.newTabSpec("我的")
+                .setIndicator(createTabIcon(R.drawable.ic_launcher, "我的"))
+                .setContent(intent3));
+        if(tab.equalsIgnoreCase("tab0")){
+            tabHost.setCurrentTabByTag("tab0");
+        }
+    }
+    
+	public View createTabIcon(int resId,String title){
+		
+		View view = null;
+		
+		view = LayoutInflater.from(this).inflate(R.layout.tab_icon, null);
+
+		ImageView iv = (ImageView)view.findViewById(R.id.iv_icon);
+		iv.setImageResource(resId);
+		
+		TextView tv = (TextView)view.findViewById(R.id.tv_title);
+		tv.setText(title);
+			
+		return view;
+	}
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
