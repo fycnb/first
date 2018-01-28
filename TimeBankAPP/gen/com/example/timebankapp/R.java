aefe221fab41aebe0d17841d4324be5c45315a27
yimoxiaohan <1044254212@qq.com>
2018-01-28 18:41:55 +0800

    1.28

--- TimeBankAPP/gen/com/example/timebankapp/R.java
+++ TimeBankAPP/gen/com/example/timebankapp/R.java
@@ -23,50 +23,18 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int img1=0x7f020001;
-        public static final int img2=0x7f020002;
-        public static final int img3=0x7f020003;
-        public static final int img4=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f08001e;
-        public static final int activityhistory_Button=0x7f08000d;
-        public static final int activityinfo_ListView=0x7f080005;
-        public static final int activityseek_Button=0x7f080001;
-        public static final int activityseek_EditText=0x7f080000;
-        public static final int cash_Button=0x7f08001c;
-        public static final int checkboxHolder=0x7f08000a;
-        public static final int clicklogin=0x7f08000c;
-        public static final int communityinfo_ImageView=0x7f08000f;
-        public static final int convertlist_ListView=0x7f08001d;
-        public static final int emptylistview_TextView=0x7f080006;
-        public static final int etPassword=0x7f080009;
-        public static final int etUsername=0x7f080008;
-        public static final int goods_Button=0x7f08001a;
-        public static final int joinactivity_ListView=0x7f080014;
-        public static final int joinactivityempty_TextView=0x7f080015;
-        public static final int loginButtonHolder=0x7f08000b;
-        public static final int message_Button=0x7f08000e;
-        public static final int publish_Button=0x7f080004;
-        public static final int publishactivity_ListView=0x7f080016;
-        public static final int publishactivityempty_TextView=0x7f080017;
-        public static final int rankchange_ImageView=0x7f080013;
-        public static final int rankimage_ImageView=0x7f080010;
-        public static final int ranknumber_TextView=0x7f080012;
-        public static final int ranktext_TextView=0x7f080011;
-        public static final int serve_Button=0x7f08001b;
-        public static final int statesequence_Button=0x7f080003;
-        public static final int textFieldHolder=0x7f080007;
-        public static final int timemoneyimage_ImageView=0x7f080018;
-        public static final int timemoneynumber_TextView=0x7f080019;
-        public static final int timesequence_Button=0x7f080002;
+        public static final int action_settings=0x7f080006;
+        public static final int checkboxHolder=0x7f080003;
+        public static final int clicklogin=0x7f080005;
+        public static final int etPassword=0x7f080002;
+        public static final int etUsername=0x7f080001;
+        public static final int loginButtonHolder=0x7f080004;
+        public static final int textFieldHolder=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_first=0x7f030000;
-        public static final int activity_four=0x7f030001;
-        public static final int activity_main=0x7f030002;
-        public static final int activity_second=0x7f030003;
-        public static final int activity_third=0x7f030004;
+        public static final int activity_main=0x7f030000;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -74,13 +42,8 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int action_sign_in=0x7f050005;
-        public static final int activityinquire=0x7f050006;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
-        public static final int title_activity_first=0x7f050007;
-        public static final int title_activity_four=0x7f05000a;
-        public static final int title_activity_second=0x7f050008;
-        public static final int title_activity_third=0x7f050009;
         public static final int user_name=0x7f050003;
         public static final int user_password=0x7f050004;
     }
